e684caa8cbc594013e00862071457d793030ac87
Raul Nair <raul.nair@uconn.edu>
2026-08-02 16:55:25 -0400

    saving changes

--- kernal/vinput_main.c
+++ kernal/vinput_main.c
@@ -1,11 +1,16 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/cdev.h>
 
 #define BUFFER_SIZE 1024
 
-static char kernel_buffer[BUFFER_SIZE];
-static size_t buffer_length = 0;
+struct my_device_data {
+    struct cdev cdev;
+    char kernel_buffer[BUFFER_SIZE];
+    size_t buffer_length = 0;
+
+};
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Raul");
@@ -18,18 +23,20 @@ static ssize_t custom_read(struct file* file_pointer,
                  size_t count, 
                  loff_t* offset){
     printk("custom_read: entry\n");
+    
+    struct my_device_data *my_data = file->private_data;
 
     if (*offset >= buffer_length){
         return 0;
     }
 
-    if (copy_to_user(user_space_buffer, kernel_buffer, buffer_length)){
+    if (copy_to_user(user_space_buffer, my_data->kernel_buffer, my_data->buffer_length)){
         return -1;
     }
 
-    *offset += buffer_length;
+    *offset += my_data->buffer_length;
 
-    return buffer_length;
+    return my_data->buffer_length;
 };
 
 static ssize_t custom_write(struct file* file_pointer, 
@@ -37,27 +44,36 @@ static ssize_t custom_write(struct file* file_pointer,
                  size_t count,
                  loff_t* offset){
     printk("custom_write: entry\n");
+    
+    struct my_device_data *my_data = file->private_data;
 
     size_t bytes_to_copy;
     bytes_to_copy = min(count, (size_t)(BUFFER_SIZE - 1));
     
-    if (copy_from_user(kernel_buffer, user_space_buffer, bytes_to_copy)){
+    if (copy_from_user(my_data->kernel_buffer, user_space_buffer, bytes_to_copy)){
         return -1;
     }
 
     kernel_buffer[bytes_to_copy] = '\0';
-    buffer_length = bytes_to_copy;
+    my_data->buffer_length = bytes_to_copy;
 
     printk("custom_write received: %s\n", kernel_buffer);
 
     return bytes_to_copy;
 };
 
-static int custom_open(struct inode* ei, struct file* file_pointer){
+static int custom_open(struct inode* inode, struct file* file_pointer){
+    struct my_device_data *my_data;
+    
+    my_data = container_of(inode->i_cdev, struct my_device_data, cdev);
+
+    file->private_data = my_data;
+
     return 0;
 };
 
-static int custom_release(struct inode* ei, struct file* file_pointer){
+static int custom_release(struct inode* inode, struct file* file_pointer){
+    file->private_data = NULL;
     return 0;
 };
 
