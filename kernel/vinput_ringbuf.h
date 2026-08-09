cb42881d7c184a18355cc4f3a1b6ad7bae10b805
Raul Nair <raul.nair@uconn.edu>
2026-08-09 17:39:32 -0400

    rest of changes

---
+++ kernel/vinput_ringbuf.h
@@ -0,0 +1,29 @@
+#ifndef _VINPUT_RINGBUF_H
+#define _VINPUT_RINGBUF_H
+
+#ifdef __KERNEL__
+#include <linux/types.h>
+#include <linux/compiler.h>
+#else
+#include <stdbool.h>
+#include <stdint.h>
+#endif
+
+#include "uapi/vinput_uapi.h"
+
+#define BUFFER_SIZE 256
+#define BUFFER_MASK (BUFFER_SIZE - 1)
+
+struct vinput_ringbuf{
+    struct vinput_event buffer[BUFFER_SIZE];
+    unsigned int head;
+    unsigned int tail;
+};
+
+void vinput_ringbuf_init(struct vinput_ringbuf *rb);
+bool vinput_ringbuf_is_empty(const struct vinput_ringbuf *rb);
+bool vinput_ringbuf_is_full(const struct vinput_ringbuf *rb);
+int vinput_ringbuf_push(struct vinput_ringbuf *rb, const struct vinput_event *event);
+int vinput_ringbuf_pop(struct vinput_ringbuf *rb, struct vinput_event *event);
+
+#endif 
